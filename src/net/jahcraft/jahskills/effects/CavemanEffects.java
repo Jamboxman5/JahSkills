63782e8a6adc4fa0dbf683f443b2f253e0f44e38
Jamboxman5 <samthompson1818@gmail.com>
2023-06-22 14:16:07 -0400

    Motherlode effect added

---
+++ src/net/jahcraft/jahskills/effects/CavemanEffects.java
@@ -0,0 +1,78 @@
+package net.jahcraft.jahskills.effects;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import net.jahcraft.jahskills.perks.Perk;
+import net.jahcraft.jahskills.skills.SkillType;
+import net.jahcraft.jahskills.skillstorage.SkillManager;
+
+public class CavemanEffects implements Listener {
+
+	SkillType type = SkillType.CAVEMAN;
+	
+	private int getRandom(int i) { return (int) (Math.random() * 101); }
+	
+	@EventHandler
+	public void motherlode(BlockBreakEvent e) {
+		
+		//INITIAL CHECKS (IS THIS EVENT ELIGIBLE FOR CONSIDERATION?)
+		
+		if (e.getPlayer() == null) return;
+		if (!SkillManager.hasPerk(e.getPlayer(), Perk.MOTHERLODE)) return;
+		if (!e.getBlock().getType().toString().contains("ORE") &&
+				!e.getBlock().getType().toString().contains("DEBRIS")) return;
+		
+		//INITIALIZE TOOLS
+		
+		Player p = e.getPlayer();
+		ItemStack tool = p.getInventory().getItemInMainHand();
+		int level = SkillManager.getLevel(p, type);
+		Collection<ItemStack> originalDrops = e.getBlock().getDrops(tool);
+		Location location = e.getBlock().getLocation();
+		World world = location.getWorld();
+
+		//SECONDARY CHECKS (IS THIS EVENT VALID FOR MANIPULATION?)
+		
+		if (originalDrops.size() == 0) return;
+		if (p.getGameMode().equals(GameMode.CREATIVE)) return;
+
+		//ROLLS (ROLL FOR CHANCE FOR PERK/MODIFIERS TO TAKE EFFECT)
+		
+		double chance = (level/100.0)*5;
+		double multiplier = 2.0;
+		
+		if (getRandom(100) > chance) return;
+		if (getRandom(100) >= 50 && level >= 20) multiplier += 1.0;
+		
+		//DO THE THING
+		
+		{
+			e.setDropItems(false);
+			List<ItemStack> newDrops = new ArrayList<>();
+			for (ItemStack i : originalDrops) {
+				i.setAmount((int)(i.getAmount() * multiplier));
+				newDrops.add(i);
+			}
+			
+			for (ItemStack i : newDrops) {
+				world.dropItemNaturally(location, i);
+			}
+		}
+		
+		//DONE!
+		
+	}
+
+	
+}
